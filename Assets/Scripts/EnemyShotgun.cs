2bf0414884ab829c79158f6464e23cbbf88789f7
Creatively Bad <creativelybad3d@gmail.com>
2022-02-19 22:28:30 -0800

    shotgun enemy fixed

--- Assets/Scripts/EnemyShotgun.cs
+++ Assets/Scripts/EnemyShotgun.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class EnemyShotgun : EnemyWeapon
 {
+    Quaternion rot;
+
     public override IEnumerator Fire(float shots, float fireRate, float loopDelay)
     {
         for (int i = 0; i < shots; i++)
         {
-            float angle = Random.Range(-45, 45);
-            Debug.Log(angle);
+            float angle = Random.Range(-20, 20);
 
-            projectile.transform.position = new Vector3(firePoint.transform.position.x,
-            firePoint.transform.position.y, firePoint.transform.position.z);
-            projectile.transform.rotation = Quaternion.Euler(0, 0, angle + transform.rotation.z);
+            Vector3 pos = firePoint.transform.position;
+
+            if (transform.localScale.x == -1)
+                rot = Quaternion.Euler(0, 0, angle + transform.localEulerAngles.z * -1);
+            else
+                rot = Quaternion.Euler(0, 0, angle + transform.localEulerAngles.z);
+
+
+            // =====> OLD CODE <=====
+            //projectile.transform.position = new Vector3(firePoint.transform.position.x,
+            //firePoint.transform.position.y, firePoint.transform.position.z);
+            //projectile.transform.rotation = Quaternion.Euler(0, 0, angle + transform.rotation.z);
 
             // spawn new projectile
-            Instantiate(projectile);
+            Instantiate(projectile, pos, rot);
 
             audioSource.PlayOneShot(shootClip);
 
