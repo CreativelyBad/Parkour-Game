0d701c4bfacc6c966e91d0189a7e77e68dfa3c47
Creatively Bad <creativelybad3d@gmail.com>
2022-02-22 22:32:41 -0800

    0.4.3 build

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,23 @@ public class MenuManager : MonoBehaviour
 {
     public TMP_Text coinCount;
 
+    public AudioSource musicSource;
+    public AudioClip[] songs;
+
     void Start()
     {
+        ChooseMusic();
+
         UnityEngine.Cursor.visible = true;
         coinCount.text = PlayerPrefs.GetInt("CoinTotal").ToString();
+    }
+
+    private void ChooseMusic()
+    {
+        int songSelected = UnityEngine.Random.Range(0, songs.Length);
+
+        musicSource.PlayOneShot(songs[songSelected]);
+
+        Debug.Log(songSelected);
     }
 }
